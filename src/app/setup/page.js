150c97cd00c3b4c67d4ae8e47ef78fd1f1ca4f75
Roberto Lozano <rlozano2@scu.edu>
2025-06-17 13:57:17 -0700

    Ready for Heroku Deploy Part 11

--- src/app/setup/page.js
+++ src/app/setup/page.js
@@ -1,7 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
 import React, { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import selectionPageStore from "../../stores/SelectionPageStore";
